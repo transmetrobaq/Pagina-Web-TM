a19e02203a81f87c3989ee4129291df70d92cb67
Manuel <ing.manuel010@gmail.com>
2023-08-09 15:52:24 -0500

    UP-EVENTOS

--- src/components/Eventos.jsx
+++ src/components/Eventos.jsx
@@ -69,6 +69,11 @@ const Eventos = () => {
       setStart('2023-07-01');
       setLimit('2023-07-31');
     }
+    // Agosto
+    if (fch === '8') {
+      setStart('2023-08-01');
+      setLimit('2023-08-31');
+    }
 
     if (fch === '11') {
       setStart('2022-11-01');
@@ -104,6 +109,7 @@ const Eventos = () => {
           <option value="5">Mayo</option>
           <option value="6">Junio</option>
           <option value="7">Julio</option>
+          <option value="8">Agosto</option>
           {/* <option value="11">Noviembre</option>
           <option value="12">Diciembre</option> */}
         </select>
