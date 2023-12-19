34e9485a23f57d0a841df46fffc7db6bff536f99
Manuel <ing.manuel010@gmail.com>
2023-12-19 10:09:41 -0500

    UP-USU-MOVILIZADO

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -138,7 +138,7 @@ const Info = () => {
                 <div className="card-body usu-movilizados ">
                   <h2 className="card-text ">Usuarios Movilizados</h2>
                   <p className="card-text">
-                    Desde el 10 de julio de 2010 hasta <br />15 de octubre de 2023
+                    Desde el 10 de julio de 2010 hasta <br /> 3 de  diciembre de 2023
                     <button
                       type="button"
                       className="btn text-white "
@@ -156,7 +156,7 @@ const Info = () => {
                     src="https://apiwebtm.com/uploads/bus_movilizados_836a10bb99.png"
                     alt="Bus Transmetro"
                   />
-                  <h1 className="card-title fw-bold">395.525.622</h1>
+                  <h1 className="card-title fw-bold">398.357.950</h1>
                   <div className="linea-colores-movi" />
                 </div>
               </div>
@@ -233,6 +233,14 @@ const Info = () => {
                   <tr>
                     <td>SEPTIEMBRE 2023 </td>
                      <td>2.023.041</td>
+                  </tr>
+                  <tr>
+                    <td>OCTUBRE 2023 </td>
+                     <td>1.850.642</td>
+                  </tr>
+                  <tr>
+                    <td> NOVIEMBRE 2023 </td>
+                     <td>1.724.582</td>
                   </tr>
 
                 </tbody>
