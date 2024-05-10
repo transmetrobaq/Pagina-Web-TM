66a16c2dbe37262f05b07a31324e323b539ed087
Manuel <ing.manuel010@gmail.com>
2024-05-10 11:33:13 -0500

    UP-CARRUSEL

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -48,7 +48,7 @@ const Header = () => {
             />
           </div> */}
 
-          <div className="carousel-item active" data-bs-interval="2000">
+          <div className="carousel-item " data-bs-interval="2000">
             <img
               src="https://apiwebtm.com/uploads/IMG_9512_76b844202b.JPG"
               className="w-100 img-fluid"
@@ -65,7 +65,7 @@ const Header = () => {
           </div>
 {/* CUANDO SE QUITE LA IMAGEN DE ARRIBA COLOCAR EL ACTIVE AL de abajo */}
 
-          <div className="carousel-item" data-bs-interval="10000">
+          <div className="carousel-item active" data-bs-interval="10000">
             <img
               src={`https://apiwebtm.com${noti1.url}`}
               className="w-100 img-fluid"
