34291b751c596b6dd45562b10d87c1dde3b7f7dd
Manuel <ing.manuel010@gmail.com>
2024-05-29 10:37:54 -0500

    UP-UsuarioMovil

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -138,7 +138,7 @@ const Info = () => {
                 <div className="card-body usu-movilizados ">
                   <h2 className="card-text ">Usuarios Movilizados</h2>
                   <p className="card-text">
-                    Desde el 10 de julio de 2010 hasta <br /> 31 de marzo  de 2024
+                    Desde el 10 de julio de 2010 hasta <br /> 19 de mayo de 2024
                     <button
                       type="button"
                       className="btn text-white "
@@ -156,7 +156,7 @@ const Info = () => {
                     src="https://apiwebtm.com/uploads/bus_movilizados_836a10bb99.png"
                     alt="Bus Transmetro"
                   />
-                  <h1 className="card-title fw-bold">404.591.828</h1>
+                  <h1 className="card-title fw-bold">407.460.028</h1>
                   <div className="linea-colores-movi" />
                 </div>
               </div>
@@ -209,6 +209,10 @@ const Info = () => {
                     <tr>
                     <td>MARZO 2024</td>
                    <td>1.652.162 </td>
+                   </tr>
+                   <tr>
+                    <td>ABRIL 2024</td>
+                   <td>1.817.671</td>
                    </tr>
                   </tbody>
               </table>
