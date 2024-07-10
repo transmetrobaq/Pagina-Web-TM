83e84ffe866c3762fb8016df63eaf39a501a6d9b
Manuel <ing.manuel010@gmail.com>
2024-07-10 11:28:44 -0500

    UP-INF-SISTEMAS

--- src/containers/Sistema.jsx
+++ src/containers/Sistema.jsx
@@ -662,7 +662,7 @@ const Sistema = () => (
           <div className="col-md-6 align-self-center">
             <div className="card-body ">
               <h2>
-                <span>68.000 Usuarios Movilizados</span> diariamente en promedio
+                <span>64.786 Usuarios Movilizados</span> diariamente en promedio
               </h2>
             </div>
           </div>
